d4aad0127a78d87da24fb10127979fd0fb657f6f
Niakr1s <pavel2188@gmail.com>
2022-11-30 17:23:16 +0300

    removed ICavernItem and extracted subclasses to separate files

--- CavernOfTime/CavernGame/Cavern/CavernItem.cs
+++ CavernOfTime/CavernGame/Cavern/CavernItem.cs
@@ -1,37 +1,12 @@
 ﻿namespace CavernOfTime
 {
-    public interface ICavernItem
-    {
-        /// <summary>
-        /// Interats with player.
-        /// </summary>
-        /// <param name="player"></param>
-        /// <returns>True, if interaction was successfull.</returns>
-        bool Interact(Player player);
-
-        bool IsActive { get; }
-    }
     /// <summary>
     /// Item, contained in Cavern.
     /// </summary>
-    public abstract class CavernItem : ICavernItem
+    public abstract class CavernItem
     {
         public virtual bool IsActive { get; } = true;
 
         public abstract bool Interact(Player player);
-    }
-
-    public class Fountain : CavernItem
-    {
-        private bool _isActive;
-        public override bool IsActive => _isActive;
-
-        public override bool Interact(Player player)
-        {
-            player.FountainVisited = true;
-            _isActive = false;
-
-            return true;
-        }
     }
 }
---
+++ CavernOfTime/CavernGame/Cavern/CavernItems/Fountain.cs
@@ -0,0 +1,16 @@
+﻿namespace CavernOfTime
+{
+    public class Fountain : CavernItem
+    {
+        private bool _isActive;
+        public override bool IsActive => _isActive;
+
+        public override bool Interact(Player player)
+        {
+            player.FountainVisited = true;
+            _isActive = false;
+
+            return true;
+        }
+    }
+}
